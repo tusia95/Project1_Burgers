6e8a0a8445a456adf89563cfcf81988d502535d5
Alina <tusia95@users.noreply.github.com>
2018-06-20 17:49:47 +0300

    1 project не получить данные из пост запроса

--- php/form.php
+++ php/form.php
@@ -3,11 +3,23 @@
  * Created by PhpStorm.
  * User: Роман Бурмистров
  * Date: 19.06.2018
- * Time: 21:55
+ * Time: 22:26
  */
-
-$mysqli = new mysqli("localhost", "root", " ", "Orders");
+$mysqli = new mysqli("localhost", "root", "", "Orders");
 if (mysqli_connect_errno()) {
     printf("Ошибка соединения: %s\n", mysqli_connect_error());
     exit();
 }
+
+if(isset($_POST['name'])) {
+    $fullName = $_POST['name'];
+    echo $fullName;
+    $name_arr = explode(" ", $fullName,2);
+    $firstname=$name_arr[0];
+    $lastname=$name_arr[1];
+
+}
+
+$sql = "INSERT INTO users (firstName, lastName) VALUES ('$firstname','$lastname')";
+$result = $mysqli->query($sql);
+var_dump ($result);
