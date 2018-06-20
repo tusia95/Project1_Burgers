1805ec76cb09a45294f9fbbd0b29d6e4c061ed5c
Alina <tusia95@users.noreply.github.com>
2018-06-20 19:30:02 +0300

    не получить данные из переменной POST

--- php/form.php
+++ php/form.php
@@ -1,25 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Роман Бурмистров
- * Date: 19.06.2018
- * Time: 22:26
- */
+
 $mysqli = new mysqli("localhost", "root", "", "Orders");
 if (mysqli_connect_errno()) {
     printf("Ошибка соединения: %s\n", mysqli_connect_error());
     exit();
 }
-
 if(isset($_POST['name'])) {
-    $fullName = $_POST['name'];
+    $fullName = $_POST['name'];//по кнопке заказать не получить значение элемента массива
     echo $fullName;
     $name_arr = explode(" ", $fullName,2);
     $firstname=$name_arr[0];
     $lastname=$name_arr[1];
 
 }
-
 $sql = "INSERT INTO users (firstName, lastName) VALUES ('$firstname','$lastname')";
 $result = $mysqli->query($sql);
-var_dump ($result);
+//var_dump ($result);//
