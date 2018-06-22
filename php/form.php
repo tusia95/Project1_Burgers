47c2badcf66818fe4b017d16c097408c1bf03d05
Alina <tusia95@users.noreply.github.com>
2018-06-22 17:30:17 +0300

    не записывается телефон и email

--- php/form.php
+++ php/form.php
@@ -1,18 +1,24 @@
 <?php
 
-$mysqli = new mysqli("localhost", "root", "", "Orders");
-if (mysqli_connect_errno()) {
-    printf("Ошибка соединения: %s\n", mysqli_connect_error());
-    exit();
-}
 if(isset($_POST['name'])) {
-    $fullName = $_POST['name'];//по кнопке заказать не получить значение элемента массива
-    echo $fullName;
-    $name_arr = explode(" ", $fullName,2);
-    $firstname=$name_arr[0];
-    $lastname=$name_arr[1];
+    $name = $_POST['name'];//по кнопке заказать не получить значение элемента массива
+   $phone_number=$_POST['phone'];
+   $email=$_POST['email'];
+    //устанавливаем соединение с БД
+    $mysqli = new mysqli("localhost", "root", "", "Burgers");
+    if (mysqli_connect_errno()) {
+        printf("Ошибка соединения: %s\n", mysqli_connect_error());
+        exit();
+    }
+        $sql = "INSERT INTO Users (name, phone_number,email) VALUES ('$name','$phone_number','email')";
+        $result = $mysqli->query($sql);
+
+    if ($result = 'true'){
+        echo "Информация занесена в базу данных";
+    }else{
+        echo "Информация не занесена в базу данных";
+    }
 
 }
-$sql = "INSERT INTO users (firstName, lastName) VALUES ('$firstname','$lastname')";
-$result = $mysqli->query($sql);
+
 //var_dump ($result);//
